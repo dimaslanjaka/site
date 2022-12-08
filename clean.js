65afb9fa33fffbdc9b3d2052cb47bcee6c0d6ae7
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-08 16:47:55 +0700

    update cleaner

--- clean.js
+++ clean.js
@@ -9,10 +9,18 @@ const toUninstall = [];
 packages.forEach(function (pkg) {
   if (/^hexo-(blogger-xml|seo|adsense|generator-redirect)$/s.test(pkg)) toUninstall.push(pkg);
 });
+/**
+ * @type {ReturnType<typeof spawn>}
+ */
+let child;
 if (toUninstall.length > 0) {
-  const child = spawn("npm", ["un", ...toUninstall], { cwd: __dirname, stdio: "inherit" });
-  child.on("close", function () {
-    console.log('reinstalling file:../../')
-    spawn("npm", ["i", "file:../../"], { cwd: __dirname, stdio: "inherit" });
-  });
+  child = spawn("npm", ["un", ...toUninstall], { cwd: __dirname, stdio: "inherit" });
+  child.on("close", reinstall);
+} else {
+  reinstall();
+}
+
+function reinstall() {
+  console.log("reinstalling file:../../");
+  spawn("npm", ["i", "file:../../"], { cwd: __dirname, stdio: "inherit" });
 }
