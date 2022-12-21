825bf5658a1cc8d7ee3d1407363b70eb181370a2
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-21 14:56:42 +0700

    update

--- list-post.test.js
+++ list-post.test.js
@@ -1,18 +1,21 @@
 process.cwd = () => __dirname;
 
 const hexoLib = require('hexo');
+const { join } = require('path');
 const { copyAllPosts } = require('..');
+const { writefile } = require('../dist/utils/fm');
 
 copyAllPosts().once('end', function () {
   const hexo = new hexoLib(__dirname);
   hexo.init().then(() => {
     hexo.load().then(() => {
       hexo.call('list', { _: ['post'] }).then(function () {
-        // ...
+        let posts = hexo.locals.get('posts').toArray().length;
+        let pages = hexo.locals.get('pages').toArray().length;
+        let tags = hexo.locals.get('tags').toArray().length;
+        let categories = hexo.locals.get('categories').toArray().length;
+        writefile(join(__dirname, 'results/list-post.json'), JSON.stringify({ posts, pages, tags, categories }));
       });
-      let posts = hexo.locals.get('posts').toArray().length;
-      let pages = hexo.locals.get('pages').toArray().length;
-      console.log({ posts, pages });
     });
   });
 });
