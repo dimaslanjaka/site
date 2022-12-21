5739fcf5d17a3022f8ddffe29bf3cd97ab3fdd50
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-21 09:16:33 +0700

    add default config auto setter

---
+++ _config.ts
@@ -0,0 +1,13 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import { getDefaultConfigYaml } from '../src/defaults';
+
+const def = getDefaultConfigYaml();
+const currentYaml = readFileSync(join(__dirname, '_config.yml'), 'utf-8');
+const regex = /(# static-blog-generator-start)([\s\S]*?)(# static-blog-generator-end)/gim;
+const subst = `$1\n${def}\n$3`;
+
+// The substituted value will be contained in the result variable
+const result = currentYaml.replace(regex, subst);
+
+writeFileSync(join(__dirname, '_config.yml'), result);
