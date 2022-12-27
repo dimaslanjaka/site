185ab2070685c68e637c220bb8a7bc316e195103
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-27 19:09:52 +0700

    update deps

--- sitemap.test.ts
+++ sitemap.test.ts
@@ -1,6 +1,15 @@
 process.cwd = () => __dirname;
 
+import { spawnAsync } from 'git-command-helper/dist/spawn';
 import { copyAllPosts, hexoGenerateSitemap } from '../src';
 import { chain } from '../src/utils/chain';
 
-chain([{ callback: copyAllPosts }, { callback: hexoGenerateSitemap }]);
+chain([
+  { callback: copyAllPosts },
+  {
+    callback: function () {
+      return spawnAsync('hexo', ['generate'], { cwd: __dirname, stdio: 'inherit' });
+    }
+  },
+  { callback: hexoGenerateSitemap }
+]);
