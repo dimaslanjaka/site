efdfac3448d6c60b3c1174357cecd13af1961331
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-02 04:25:45 +0700

    hotfix label mapper

--- _3_copy-mapper.ts
+++ _3_copy-mapper.ts
@@ -4,7 +4,13 @@ import { Application, copyAllPosts } from '../src';
 import { writefile } from '../src/utils/fm';
 
 const results = join(__dirname, 'results');
-const api = new Application(__dirname);
-api.setConfig({ tags: { lowercase: true } });
-writefile(join(results, 'copy-mapper.json'), JSON.stringify(api.getConfig(), null, 2));
-copyAllPosts();
+(async function () {
+  const api = new Application(__dirname);
+  api.setConfig({
+    tags: { lowercase: true, mapper: { TS: 'typescript', JS: 'javascript' } },
+    categories: { lowercase: true, mapper: { TS: 'typescript', JS: 'javascript' } }
+  });
+  await api.clean();
+  writefile(join(results, 'copy-mapper.json'), JSON.stringify(api.getConfig(), null, 2));
+  copyAllPosts();
+})();
---
+++ src-posts/label-mapper.md
@@ -0,0 +1,11 @@
+---
+title: Label Mapper
+tags: ['JS', 'TS']
+category: ['JS', 'TS']
+---
+
+Label mapper
+
+JS to javascript
+
+TS to typescript
