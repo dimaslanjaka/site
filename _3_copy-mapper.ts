071561b76a285eb8df2405050cdf078cdc73b15b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-02 04:33:51 +0700

    initialize label assigner

--- _3_copy-mapper.ts
+++ _3_copy-mapper.ts
@@ -7,8 +7,12 @@ const results = join(__dirname, 'results');
 (async function () {
   const api = new Application(__dirname);
   api.setConfig({
-    tags: { lowercase: true, mapper: { TS: 'typescript', JS: 'javascript' } },
-    categories: { lowercase: true, mapper: { TS: 'typescript', JS: 'javascript' } }
+    tags: { lowercase: true, mapper: { TS: 'typescript', JS: 'javascript' }, assign: { code: 'javascript' } },
+    categories: {
+      lowercase: true,
+      mapper: { TS: 'typescript', JS: 'javascript' },
+      assign: { programming: 'javascript' }
+    }
   });
   await api.clean();
   writefile(join(results, 'copy-mapper.json'), JSON.stringify(api.getConfig(), null, 2));
