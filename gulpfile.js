cf1f3b2f6264b5bef4e918063218660f283bb31b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-02 15:17:20 +0700

    update test

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
+process.cwd = () => __dirname;
+process.env.NODE_ENV = 'dev';
+
 const path = require('path');
-const { gulpDebug, gulp } = require('static-blog-generator');
+const through2 = require('through2');
+const gulp = require('gulp');
+const { gulpCached, gulpDebug } = require('../dist');
 
 gulp.task('test', function () {
   return gulp
     .src('**/*.*', { cwd: path.join(__dirname, 'src-posts') })
     .pipe(gulpDebug())
     .pipe(gulp.dest(path.join(__dirname, 'tmp/test')));
+});
+
+gulp.task('test:cache', () => {
+  const dest = path.join(__dirname, 'source/_posts');
+  const cwd = path.join(__dirname, '/src-posts');
+  return gulp
+    .src('**/*.*', { cwd })
+    .pipe(gulpCached({ name: 'test-gulp-cache' }))
+    .pipe(
+      through2.obj(function (file, _enc, next) {
+        file.extname = '.html';
+        next(null, file);
+      })
+    )
+    .pipe(gulp.dest(dest));
 });
