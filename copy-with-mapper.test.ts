2ff832669801e9ca6689207c2842a31c74983d7a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 16:28:25 +0700

    using nest describe-test

--- copy-with-mapper.test.ts
+++ copy-with-mapper.test.ts
@@ -1,39 +1,34 @@
 process.cwd = () => __dirname;
 
-import { beforeEach, describe, expect, test } from '@jest/globals';
-import { existsSync } from 'fs';
+import { describe, expect, test } from '@jest/globals';
 import { parsePost } from 'hexo-post-parser';
 import { join } from 'upath';
 import { Application } from '../src';
 import validateClean from './validate-clean';
+import validateCopy from './validate-copy';
 
 describe('test copy post with label mapper', function () {
-  let api: Application;
-  beforeEach(function () {
-    api = new Application(__dirname, {
-      generator: {
-        cache: false,
-        verbose: false,
-        test: true
-      },
-      exclude: [],
-      permalink: '/:month/:title/',
-      tags: { lowercase: true, mapper: { TS: 'typescript', JS: 'javascript' }, assign: { code: 'snippet' } },
-      categories: {
-        lowercase: true,
-        mapper: { TS: 'typescript', JS: 'javascript' },
-        assign: { programming: 'javascript' }
-      }
-    });
+  const api = new Application(__dirname, {
+    generator: {
+      cache: false,
+      verbose: false,
+      test: true
+    },
+    exclude: [],
+    permalink: '/:month/:title/',
+    tags: { lowercase: true, mapper: { TS: 'typescript', JS: 'javascript' }, assign: { code: 'snippet' } },
+    categories: {
+      lowercase: true,
+      mapper: { TS: 'typescript', JS: 'javascript' },
+      assign: { programming: 'javascript' }
+    }
   });
-  if (existsSync(join(__dirname, 'tmp'))) {
-    test('clean', (done) => {
-      api.clean().then(() => validateClean(api, done));
-    }, 60000);
-  }
-  test('copying', async () => {
-    await api.copy();
-  }, 60000);
+  describe('clean', () => {
+    return validateClean(api);
+  });
+  describe('copy', () => {
+    return validateCopy(api);
+  });
   test('label assign (add label based on config.tags|categories.assign key)', async () => {
     const postAssign = join(__dirname, 'source/_posts/label-assigner.md');
     const parsePostAssign = await parsePost(postAssign, {
