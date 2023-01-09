256a91f526d79e2e2c90d61dc961ae2d0c4a96b7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-09 16:52:08 +0700

    run build before run dist

--- copy-dist.js
+++ copy-dist.js
@@ -1,16 +1,19 @@
 process.cwd = () => __dirname;
 process.env.DEBUG = 'hexo-post-parser,post:permalink';
 
+const promiseSpawn = require('hexo-util/dist/spawn');
 const { Application } = require('..');
 const { chain } = require('../dist/utils/chain');
 
 const api = new Application(__dirname);
 chain([
   {
-    callback: function () {
-      api.setConfig({ permalink: ':title/' });
-    }
+    callback: () => promiseSpawn('npm', ['run', 'build:nopack'], { cwd: __dirname + '/../' })
   },
-  { callback: api.clean },
-  { callback: api.copy }
+  {
+    callback: function () {
+      api.setConfig({ permalink: ':name/' });
+      return chain([{ callback: api.clean }, { callback: api.copy }]);
+    }
+  }
 ]);
