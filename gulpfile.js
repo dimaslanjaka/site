6aa940efab2c60707e70980d017ff3a3ac714bd2
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-20 04:07:06 +0700

    update test

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,6 @@ process.cwd = () => __dirname;
 process.env.NODE_ENV = 'dev';
 
 const path = require('path');
-const gulp = require('gulp');
-const { gulpCached, gulpDebug, copyAllPosts } = require('../dist');
-const { cleanOldArchives, cleanDb } = require('../dist/gulp.clean');
-const { chain } = require('../dist/utils/chain');
-require('sbg-main');
+const gulp = require('static-blog-generator');
 
-gulp.task('test:debug', function () {
-  return gulp
-    .src('**/*.*', { cwd: path.join(__dirname, 'src-posts') })
-    .pipe(gulpDebug())
-    .pipe(gulp.dest(path.join(__dirname, 'tmp/test')));
-});
-
-const testCached = () => {
-  const dest = path.join(__dirname, 'tmp/_posts');
-  const cwd = path.join(__dirname, '/src-posts');
-  return gulp
-    .src('**/*.*', { cwd })
-    .pipe(gulpCached({ name: 'test-gulp-cache' }))
-    .pipe(gulp.dest(dest));
-};
-const testNotCached = () => {
-  return chain([{ callback: cleanDb }, { callback: testCached }]);
-};
-
-gulp.task('test:cache', gulp.series([testNotCached, testCached]));
-
-gulp.task('test:clean-archive', function () {
-  return chain([{ callback: cleanDb }, { callback: copyAllPosts }, { callback: cleanOldArchives }]);
-});
+console.log(gulp);
