11ec2e3d97552a8b8aa87fb43184a6e6204f56f8
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-20 14:18:55 +0700

    update install-local.js

--- install-local.js
+++ install-local.js
@@ -1,9 +1,10 @@
 const { spawnAsync } = require('git-command-helper/dist/spawn');
 const path = require('path');
-
+const pkg = require('../package.json')
 const dist = path.join(__dirname, '../');
 
 (async function () {
+  await spawnAsync('npm', ['install'], { cwd: dist, stdio: 'inherit' });
   await spawnAsync('npm', ['pack'], { cwd: dist, stdio: 'inherit' });
-  await spawnAsync('npm', ['i', '-D', 'file:../static-blog-generator-3.0.0.tgz'], { cwd: __dirname, stdio: 'inherit' });
+  await spawnAsync('npm', ['i', '-D', `file:../${pkg.name}-${pkg.version}.tgz`], { cwd: __dirname, stdio: 'inherit' });
 })();
