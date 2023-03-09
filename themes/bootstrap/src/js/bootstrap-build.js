dd0952cb04e7401f9432bee51b07f4a6a2a1f74a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-09 20:34:06 +0700

    hotfix bootstrap js using rollup

---
+++ themes/bootstrap/src/js/bootstrap-build.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as Popper from '../../node_modules/@popperjs/core/dist/umd/popper.js';
+
+// from 'js/src/*' source which works
+import Modal from '../../node_modules/bootstrap/js/src/modal';
+import Tab from '../../node_modules/bootstrap/js/src/tab';
+import Dropdown from '../../node_modules/bootstrap/js/src/dropdown';
+import Tooltip from '../../node_modules/bootstrap/js/src/tooltip';
+
+export default {
+  // Alert,
+  // Button,
+  // Carousel,
+  // Collapse,
+  Dropdown,
+  Modal,
+  // Offcanvas,
+  // Popover,
+  // ScrollSpy,
+  Tab,
+  // Toast,
+  // Popper,
+  Tooltip
+};
