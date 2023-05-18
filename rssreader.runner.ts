b2c7d0e6b6c6c387bfbbe2db55481e1bf254f051
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-18 13:50:36 +0700

    chore: hotfix get thumbnail from rss item

--- rssreader.runner.ts
+++ rssreader.runner.ts
@@ -4,22 +4,22 @@ import { rssreader } from '../lib/src/rssreader';
 import fs from 'fs';
 import path from 'path';
 
+const tmp = path.join(__dirname, '../tmp');
+if (!fs.existsSync(tmp)) fs.mkdirSync(tmp, { recursive: true });
+
 const hexo = new Hexo(__dirname);
 hexo.init().then(() => {
   const { callback } = rssreader(hexo);
   callback(
-    ['https://www.webmanajemen.com/rss.xml'],
+    ['https://www.kompiajaib.com/feeds/posts/default', 'limit:10'],
     `
-<div style="margin-bottom: 7px; padding: 7px">
-  <div style="display: flex">
-    <div>$image</div>
-    <div><h2>$title</h2></div>
-  </div>
-  <p>$content</p>
-  <a href="$link" rel="follow">Read More</a>
-</div>
-  `
+## $title
+> published $date
+![$title]($image)
+
+[read more]($link)
+  `.trim()
   ).then((result) => {
-    fs.writeFileSync(path.join(__dirname, '../tmp/rss.html'), result);
+    fs.writeFileSync(path.join(tmp, 'rss.md'), result);
   });
 });
