109ef889d332953e29bc6cef10ff9c9671486633
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-19 15:37:03 +0700

    fix: test render

--- test-lang.ts
+++ test-lang.ts
@@ -6,8 +6,9 @@ import path from 'path';
 const hexo = new Hexo(__dirname);
 async function main() {
   await hexo.init();
+  hexo.init().then(() => hexo.loadPlugin(require.resolve('hexo-shortcodes')));
   await hexo.load(async () => {
-    const post = await hexo.render.render({ path: path.join(__dirname, 'source/lang/php.md') });
+    const post = await hexo.post.render(path.join(__dirname, 'source/lang/php.md'));
     console.log(post);
   });
 }
