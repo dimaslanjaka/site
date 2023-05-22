7f0aa5fb29b009e1535883b21e15828d9fda3cf4
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-22 23:38:52 +0700

    feat: add custom layout copier

--- _config.loader.js
+++ _config.loader.js
@@ -1,4 +1,5 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { copySync } = require('fs-extra');
+const { readFileSync, writeFileSync, readdirSync } = require('fs-extra');
 const { join } = require('path');
 const { parse, stringify } = require('yaml');
 
@@ -12,9 +13,23 @@ function reloadHexoConfigYml(base = undefined, overriden = {}) {
   if (!base) base = __dirname;
   if (!overriden) overriden = {};
   const defaults = parse(readFileSync(join(base, '_config.defaults.yml'), 'utf-8'));
+  /** @type {import('hexo')['config']} */
   const options = Object.assign(defaults, overriden);
   writeFileSync(join(base, '_config.yml'), stringify(options));
+  const layouts = readdirSync(join(__dirname, 'views')).map((str) => {
+    return {
+      src: join(join(__dirname, 'views'), str),
+      dest: join(join(__dirname, 'themes', options.theme), str)
+    };
+  });
+  layouts.forEach(({ src, dest }) => {
+    copySync(src, dest);
+  });
   return options;
 }
 
 module.exports = reloadHexoConfigYml;
+
+if (require.main === module) {
+  reloadHexoConfigYml(__dirname, { theme: 'butterfly' });
+}
